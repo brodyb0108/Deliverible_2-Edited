afbf91de2dbd80e1ac8cb951c173d5fd664c81c4
Brody <Brody@DESKTOP-S685LHF>
2023-07-10 15:43:34 -0400

    Add project files.

---
+++ Deliverible_2 Edited/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Deliverable_2_Edited
+{
+	class Program
+	{
+		static int[] random_array(int array_len)
+		{
+			Random random = new Random();
+			int[] MyArray = new int[array_len];
+			for (int i = 0; i < array_len; i++)
+			{
+				MyArray[i] = random.Next(10, 50);
+			}
+			return MyArray;
+		}
+
+		static int sum(int[] array)
+		{
+			int sum = 0;
+			foreach (int item in array)
+			{
+				sum = sum + item;
+			}
+			return sum;
+		}
+
+		static void Main(string[] args)
+		{
+			try
+			{
+
+				Console.WriteLine("Enter an integer between 5 and 15");
+				int input = int.Parse(Console.ReadLine());
+
+				int[] sampleArray = random_array(input);
+				Console.WriteLine("The elements in the array are: ");
+				foreach (int element in sampleArray)
+				{
+					Console.Write(element + " ");
+				}
+				Console.WriteLine(" ");
+				Console.WriteLine("The sum is: " + sum(sampleArray));
+			}
+			catch
+			{
+				Console.WriteLine("Please enter an integer between 5 and 15");
+			}
+		}
+	}
+}
